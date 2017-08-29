1d960b77d6ca282c735e232fab904c8c87aa8f3a
vansjyo <vanshika421@gmail.com>
2017-08-29 16:12:40 +0530

    files changed-css js index

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+              $(window).load(function() {
+                $('.flexslider').flexslider();
+              });
+
+
+               $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+          }
+          );
+ 
+
+
